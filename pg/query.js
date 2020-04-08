7cff8ee5299d895bc60cc225f83808fafd8e3d27
nsbernstein52 <nsbernstein52@gmail.com>
2020-04-07 19:44:41 -0600

    Create query.js

--- pg/query.js
+++ pg/query.js
@@ -2,24 +2,31 @@ const { Pool, Client } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   database: 'productsdb',
-  password: 'deimos',
+  password: 'Psa2020s',
   port: 5432,
 })
+
+console.log( new Date());
+console.log("q.js: ENTERING");
 
 pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   pool.end()
 })
 
-// const client = new Client({
-//   user: 'postgres',
-//   database: 'productsdb',
-//   password: 'deimos',
-//   port: 5432,
-// })
+const getProduct = (id) => {
+  let values = [id];
+  console.log("q: gP: id ENTERED", id);
+  return pool.query("SELECT * FROM products where id = $1", values)
+  .then(res => {
+    console.log("q: gP r.r[0]:", res.rows[0]);
+    return res.rows[0];
+  })
+};
 
-// client.connect()
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   client.end()
-// })
+console.log("q.js: LEAVING");
+
+module exports = {
+  getProduct
+}
+
