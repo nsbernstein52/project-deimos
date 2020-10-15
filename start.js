0c6926c892bf3f39037b4e4aac5535d5a5980eb1
nsbernstein52 <nsbernstein52@gmail.com>
2020-10-15 11:40:37 -0600

    Change log to say "Start" & listening

--- start.js
+++ start.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Web server running on: http://localhost:${PORT}, at`, new Date().toLocaleTimeString()); // eslint-disable-line
+  console.log(`Starting web server listening on: http://localhost:${PORT}, at`, new Date().toLocaleTimeString()); // eslint-disable-line
 });
